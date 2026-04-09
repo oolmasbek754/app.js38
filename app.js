bda6ac4e144be0881f21622c930b5c7826a6788c
Odilbekov O`lmasbek <oolmasbek754@gmail.com>
2026-04-09 15:12:01 +0500

    Implement points function to calculate game scores

---
+++ app.js
@@ -0,0 +1,16 @@
+function points(games) {
+  let a = 0
+  for(key of games) {
+    let game1 = key.split(":")
+    let ourScore = +game1.slice(0, 1)
+    let theirScore = +game1.slice(-1)
+    if(ourScore > theirScore) {
+      a += 3
+    } else if(ourScore == theirScore) {
+      a += 1
+    } else {
+      a += 0
+    }
+  }
+  return a
+}
